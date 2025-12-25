bcd49bcb95ec0c1dcd22f51fc1c26c0386c4251e
Jackob2004 <zjakub2004@gmail.com>
2025-12-25 13:59:57 +0100

    Implement growing snake

--- snake.c
+++ snake.c
@@ -24,3 +24,29 @@ snake_t *create_snake(const double speed, const double x_start, const double y_s
     return snake;
 }
 
+void grow_snake(snake_t *snake) {
+    if (snake->length == snake->capacity) {
+        snake->capacity *= 2;
+        body_part_t **temp= realloc(snake->body, sizeof(body_part_t) * snake->capacity);
+
+        if (temp == NULL) {
+            exit(EXIT_FAILURE);
+        }
+
+        snake->body = temp;
+    }
+
+    body_part_t *body_part = malloc(sizeof(body_part_t));
+    if (snake->body == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    const body_part_t *next = snake->body[snake->length - 1];
+    body_part->symbol = '*';
+    body_part->x_direction = next->x_direction;
+    body_part->y_direction = next->y_direction;
+    body_part->x = next->x - next->x_direction;
+    body_part->y = next->y - next->y_direction;
+
+    snake->body[snake->length] = body_part;
+    snake->length++;
+}
