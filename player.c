c8b6e54d00afc3f1d5d0a84f484ed0f247a98cfc
Jackob2004 <zjakub2004@gmail.com>
2025-12-31 17:36:58 +0100

    Implement loading players data
    Add loading players data from csv file.

--- player.c
+++ player.c
@@ -50,3 +50,79 @@ char *get_player_name() {
 
     return name;
 }
+
+player_t player_from_csv_str(const char *csv_str) {
+    player_t player;
+
+    int idx = 0;
+    char *name = malloc(sizeof(char) * MAX_NAME_LENGTH + 1);
+    while (csv_str[idx] != ',') {
+        name[idx] = csv_str[idx];
+        idx++;
+    }
+
+    name[idx] = '\0';
+    idx++;
+
+    char number[10];
+    char number_idx = 0;
+    while (csv_str[idx] != '\0') {
+        number[number_idx] = csv_str[idx];
+        idx++;
+        number_idx++;
+    }
+    number[number_idx] = '\0';
+
+    player.name = name;
+    player.score = atoi(number);
+
+    return player;
+}
+
+players_t *create_players() {
+    players_t *players = malloc(sizeof(players_t));
+
+    if (players == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    players->length = 0;
+    players->capacity = 10;
+    players->items = malloc(sizeof(player_t) * players->capacity);
+
+    if (players->items == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    return players;
+}
+
+void destroy_players(players_t *players) {
+    for (int i = 0; i < players->length; i++) {
+        free(players->items[i].name);
+    }
+    free(players->items);
+    free(players);
+}
+
+players_t *load_players() {
+    players_t *players = create_players();
+
+    FILE *file = fopen(STATS_FILE_NAME, "r");
+
+    if (file == NULL) {
+        fopen(STATS_FILE_NAME, "w");
+        file = fopen(STATS_FILE_NAME, "r");
+    }
+
+    char line[20];
+
+    while (fgets(line, 20, file)) {
+        line[strcspn(line, "\n")] = 0;
+        array_append(players, player_from_csv_str(line));
+    }
+
+    fclose(file);
+
+    return players;
+}
--- player.h
+++ player.h
@@ -3,13 +3,22 @@
 
 #include "ncurses.h"
 #include "stdlib.h"
+#include "array.h"
 
 #define MAX_NAME_LENGTH 10
+#define STATS_FILE_NAME "player.csv"
 
 typedef struct Player {
     char *name;
     int score;
 } player_t;
 
+typedef struct Players {
+    size_t length;
+    size_t capacity;
+    player_t *items;
+} players_t;
+
 char *get_player_name();
+void save_player_best_score(player_t player);
 #endif //SNAKE_PLAYER_H
