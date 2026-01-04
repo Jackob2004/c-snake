4127b317f622f071d30a39255eb6cc78caf7b6ed
Jackob2004 <zjakub2004@gmail.com>
2026-01-04 19:58:01 +0100

    Add snake title visual
    Display asci style "SNAKE" in the main menu.

--- main-menu.c
+++ main-menu.c
@@ -2,6 +2,27 @@
 
 #include "scoreboard.h"
 
+static char *game_title[] = {
+    "   _____   _   _          _  __   ______  ",
+    "  / ____| | \\ | |   /\\   | |/ /  |  ____| ",
+    " | (___   |  \\| |  /  \\  | ' /   | |__    ",
+    "  \\___ \\  | . ` | / /\\ \\ |  <    |  __|   ",
+    "  ____) | | |\\  |/ ____ \\| . \\   | |____  ",
+    " |_____/  |_| \\_/_/    \\_\\_|\\_\\  |______| "
+};
+
+void display_game_title() {
+    const int rows = sizeof(game_title) / sizeof(game_title[0]);
+    point_t point = calc_middle_position((int)strlen(game_title[0]), rows, stdscr);
+    point.y = point.y / 4 + rows;
+
+    for (int i = 0; i < rows; i++) {
+        mvprintw(point.y + i, point.x, "%s", game_title[i]);
+    }
+
+    refresh();
+}
+
 void scoreboard(void *ptr) {
     open_scoreboard(NULL);
 }
@@ -26,6 +47,8 @@ void stop() {
 }
 
 void main_menu(void *ptr) {
+    display_game_title();
+
     const int COLUMNS = 80;
     const point_t spawn_point = calc_middle_position(COLUMNS, 5, stdscr);
     char *options[] = {"Start", "Scoreboard", "Exit"};
--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
+#include "game.h"
+#include "main-menu.h"
 
 int main(void) {
+    srand(time(NULL));
     initscr();
-
-    printw("Hello World!");
+    cbreak();
+    noecho();
+    curs_set(false);
+    start_color();
     refresh();
+
+    main_menu(NULL);
+
     getch();
 
     endwin();
